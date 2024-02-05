6fbdff61d385f9b88fbc48eadad33f03f99599a2
Timothy McMichael <timmcmic@microsoft.com>
2024-02-05 15:09:51 -0500

    Updating

---
+++ reset-EntraConnectObject.ps1
@@ -0,0 +1,41 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 273a5e29-2a69-4872-a2b4-f4e17458aab1
+
+.AUTHOR timmcmic@microsoft.com
+
+.COMPANYNAME Microsoft
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ This purges connector space objects and performs a single item sync. 
+
+#> 
+Param()
+
+
