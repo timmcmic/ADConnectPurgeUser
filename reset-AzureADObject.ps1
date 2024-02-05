cec2b43a752098c918285a09d5ddba0d13453ca7
Timothy McMichael <timmcmic@microsoft.com>
2024-02-05 11:00:49 -0500

    Updating

--- reset-AzureADObject.ps1
+++ reset-AzureADObject.ps1
@@ -1,29 +1,47 @@
 
-#############################################################################################
-# DISCLAIMER:																				#
-#																							#
-# THE SAMPLE SCRIPTS ARE NOT SUPPORTED UNDER ANY MICROSOFT STANDARD SUPPORT					#
-# PROGRAM OR SERVICE. THE SAMPLE SCRIPTS ARE PROVIDED AS IS WITHOUT WARRANTY				#
-# OF ANY KIND. MICROSOFT FURTHER DISCLAIMS ALL IMPLIED WARRANTIES INCLUDING, WITHOUT		#
-# LIMITATION, ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR OF FITNESS FOR A PARTICULAR		#
-# PURPOSE. THE ENTIRE RISK ARISING OUT OF THE USE OR PERFORMANCE OF THE SAMPLE SCRIPTS		#
-# AND DOCUMENTATION REMAINS WITH YOU. IN NO EVENT SHALL MICROSOFT, ITS AUTHORS, OR			#
-# ANYONE ELSE INVOLVED IN THE CREATION, PRODUCTION, OR DELIVERY OF THE SCRIPTS BE LIABLE	#
-# FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF BUSINESS	#
-# PROFITS, BUSINESS INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS)	#
-# ARISING OUT OF THE USE OF OR INABILITY TO USE THE SAMPLE SCRIPTS OR DOCUMENTATION,		#
-# EVEN IF MICROSOFT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES						#
-#############################################################################################
+<#PSScriptInfo
 
-function reset-AzureADObject
-{
-    [cmdletbinding()]
+.VERSION 1.0
 
-    Param
-    (
-        [Parameter(Mandatory = $false)]
-        [string]$objectMailAddress
-    )
+.GUID 73682885-3755-49b2-abbe-6ddfe39cbbaf
 
-    write-host "Hello"
-}
+.AUTHOR timmcmic@microsoft.com
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ This script allows a user to find an object and purge single objects from AD Connect and resync. 
+
+#> 
+[cmdletbinding()]
+
+Param
+(
+    [Parameter(Mandatory = $false)]
+    [string]$objectMailAddress
+)
+
+
